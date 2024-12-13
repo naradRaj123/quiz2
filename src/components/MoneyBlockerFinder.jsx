372cb7e475cb26d694b6b4aa4a2164fd5c35f884
naradraj123 <naradrajbhargzp123@gmail.com>
2024-12-13 16:20:50 +0530

    sdfs

--- src/components/MoneyBlockerFinder.jsx
+++ src/components/MoneyBlockerFinder.jsx
@@ -1,64 +1,108 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 
 function AllUsers() {
+
+    const [userData, setuserData] = useState([]);
+    const getUserData = async () => {
+        await axios.get(`https://quiz-app-x6vq.onrender.com/studentlist`)
+            .then((resp) => {
+                console.log(resp.data.userdata);
+                setuserData(resp.data.userdata);
+            })
+    }
+
+    useEffect(() => {
+        getUserData();
+    }, [])
+
     return (
         <>
             <main>
                 <div className="customContainer bg-white p-5 rounded-lg mx-auto shadow-sm">
                     <h1 className="text-lg font-semibold col-span-2 border-neutral-200 pb-2">All Register User</h1>
                     {/* <div className='grid grid-cols-6 gap-2'>
-                        <div className="w-full flex flex-col gap-1">
-                            <label className="font-semibold text-xs text-gray-500">Name</label>
-                            <input type="text" className="border rounded-lg px-3 py-2 text-sm w-full outline-none dark:border-gray-200 dark:bg-gray-100" placeholder="John Doe" />
-                        </div>
-                        <div className="w-full flex flex-col gap-1">
-                            <label className="font-semibold text-xs text-gray-500">PAN Card No.</label>
-                            <input type="text" className="border rounded-lg px-3 py-2 text-sm w-full outline-none dark:border-gray-200 dark:bg-gray-100" placeholder="DSKPA123456" />
-                        </div>
-                        <div className="w-full flex flex-col gap-1">
-                            <label className="font-semibold text-xs text-gray-500">Aadhar Card No.</label>
-                            <input type="text" className="border rounded-lg px-3 py-2 text-sm w-full outline-none dark:border-gray-200 dark:bg-gray-100" placeholder="12345678905454" />
-                        </div>
-                        <div className="w-full flex flex-col gap-1">
-                            <label className="font-semibold text-xs text-gray-500">GST No.</label>
-                            <input type="text" className="border rounded-lg px-3 py-2 text-sm w-full outline-none dark:border-gray-200 dark:bg-gray-100" placeholder="22AAAAA0000A1Z5" />
-                        </div>
-                        <div className="w-full flex flex-col gap-1">
-                            <label className="font-semibold text-xs text-gray-500">Mobile</label>
-                            <input type="text" className="border rounded-lg px-3 py-2 text-sm w-full outline-none dark:border-gray-200 dark:bg-gray-100" placeholder="9876543210" />
-                        </div>
-                        <div className="col-span-5">
-                            <button className="h-full border-2 text-gray-400 font-bold py-2 px-4 rounded-md"><i className="fa-solid fa-magnifying-glass"></i></button>
-                        </div>
-                    </div> */}
+            <div className="w-full flex flex-col gap-1">
+            <label className="font-semibold text-xs text-gray-500">Name</label>
+            <input type="text" className="border rounded-lg px-3 py-2 text-sm w-full outline-none dark:border-gray-200 dark:bg-gray-100" placeholder="John Doe" />
+            </div>
+            <div className="w-full flex flex-col gap-1">
+            <label className="font-semibold text-xs text-gray-500">PAN Card No.</label>
+            <input type="text" className="border rounded-lg px-3 py-2 text-sm w-full outline-none dark:border-gray-200 dark:bg-gray-100" placeholder="DSKPA123456" />
+            </div>
+            <div className="w-full flex flex-col gap-1">
+            <label className="font-semibold text-xs text-gray-500">Aadhar Card No.</label>
+            <input type="text" className="border rounded-lg px-3 py-2 text-sm w-full outline-none dark:border-gray-200 dark:bg-gray-100" placeholder="12345678905454" />
+            </div>
+            <div className="w-full flex flex-col gap-1">
+            <label className="font-semibold text-xs text-gray-500">GST No.</label>
+            <input type="text" className="border rounded-lg px-3 py-2 text-sm w-full outline-none dark:border-gray-200 dark:bg-gray-100" placeholder="22AAAAA0000A1Z5" />
+            </div>
+            <div className="w-full flex flex-col gap-1">
+            <label className="font-semibold text-xs text-gray-500">Mobile</label>
+            <input type="text" className="border rounded-lg px-3 py-2 text-sm w-full outline-none dark:border-gray-200 dark:bg-gray-100" placeholder="9876543210" />
+            </div>
+            <div className="col-span-5">
+            <button className="h-full border-2 text-gray-400 font-bold py-2 px-4 rounded-md"><i className="fa-solid fa-magnifying-glass"></i></button>
+            </div>
+            </div> */}
                 </div>
                 <div className="customContainer  flex items-center bg-white justify-center  p-5 rounded-lg mx-auto shadow-sm mt-5">
-                    
+
                     <div className=" w-full   ">
-                    {/* <p className="text-sm">No data found</p> */}
-                    <div className="grid gap-2">
-                            <Link to={'#'} className="relative text-sm border rounded-lg shadow-md p-4 grid lg:grid-cols-3 md:grid-cols-3 grid-cols-1 gap-1 hover:translate-x-2 duration-200">
-                                <div className="space-y-1">
-                                    <p><span className="font-medium text-neutral-400 me-2">Name: </span>Lorem Ipsum</p>
-                                    <p><span className="font-medium text-neutral-400 me-2">Mobile: </span>1234567890</p>
-                                </div>
-                                <div className="space-y-1">
-                                    <p><span className="font-medium text-neutral-400 me-2">Email: </span>user@gmail.com</p>
-                                    <p><span className="font-medium text-neutral-400 me-2">Gender: </span>Male </p>
-                                </div>
-                                
-                                <div className="space-y-1">
-                                    <p><span className="font-medium text-neutral-400 me-2">Qualification: </span>12th</p>
-                                    <p className="flex items-center " ><span className="font-medium  text-neutral-400 me-2">Image: </span><img src="/img/fraud.png" alt="" className="w-24" /></p>
-                                </div>
-                                <div className="space-y-1">
-                                    <p className="text-blue text-bold "><span className="font-medium text-neutral-400 me-2">Action: </span>Edit</p>
-                                    <p><span className="font-medium text-neutral-400 me-2">Status: </span>Active </p>
-                                </div>
-                            </Link>
+                        {/* <p className="text-sm">No data found</p> */}
+                        <div className="overflow-x-auto">
+                            <table className="table-auto w-full border-collapse border border-gray-300 shadow-md">
+                                <thead>
+                                    <tr className="bg-gray-100 text-left">
+                                        <th className="border border-gray-300 px-4 py-2">Name</th>
+                                        <th className="border border-gray-300 px-4 py-2">Mobile</th>
+                                        <th className="border border-gray-300 px-4 py-2">Email</th>
+                                        <th className="border border-gray-300 px-4 py-2">Gender</th>
+                                        <th className="border border-gray-300 px-4 py-2">Qualification</th>
+                                        <th className="border border-gray-300 px-4 py-2">Image</th>
+                                        <th className="border border-gray-300 px-4 py-2">Action</th>
+                                        <th className="border border-gray-300 px-4 py-2">Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {userData.length ? (
+                                        userData.map((v, i) => {
+                                            return (
+                                                <>
+                                                    <tr className="hover:bg-gray-50 duration-200">
+                                                        <td className="border border-gray-300 px-4 py-2">{v.name}</td>
+                                                        <td className="border border-gray-300 px-4 py-2">{v.mobileNo}</td>
+                                                        <td className="border border-gray-300 px-4 py-2">{v.email}</td>
+                                                        <td className="border border-gray-300 px-4 py-2">{v.gender}</td>
+                                                        <td className="border border-gray-300 px-4 py-2">{v.qualification}</td>
+                                                        <td className="border border-gray-300 px-4 py-2">
+                                                            <img src="/img/fraud.png" alt="User" className="w-24" />
+                                                        </td>
+                                                        <td className="border border-gray-300 px-4 py-2 text-blue-600 font-bold cursor-pointer">
+                                                            Edit
+                                                        </td>
+                                                        <td className="border border-gray-300 px-4 py-2">Active</td>
+                                                    </tr>
+                                                </>
+                                            )
+                                        })
 
+                                    ) : (
+                                        <>
+                                            <tr className="w-full">
+                                                <td colSpan="8" className="border border-gray-300 px-4 py-2 text-center">
+                                                    No data available
+                                                </td>
+                                            </tr>
+                                        </>
+                                    )}
 
+                                </tbody>
+                            </table>
                         </div>
+
                     </div>
                 </div>
             </main>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,9 +5,9 @@ function Login() {
     
     return (
         <>
-            <main className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900 px-4 sm:px-6 lg:px-8">
+            <main className="min-h-screen flex items-center justify-center bg-gray-900 dark:bg-gray-900 px-4 sm:px-6 lg:px-8">
                 <div className="relative ">
-                    <div className="w-[320px] min-h-96 px-8 py-6 mt-4 text-left bg-white dark:bg-gray-800 dark:border-gray-700 rounded-xl shadow-lg">
+                    <div className="w-[320px] min-h-96 px-8 py-6 mt-4 text-left bg-gray-200 dark:bg-gray-800 dark:border-gray-700 rounded-xl shadow-lg">
                         <div className="flex flex-col justify-center items-center h-full select-none">
                             <div className="flex flex-col items-center justify-center gap-2 mb-8">
                                 <a href="" target="_blank">
